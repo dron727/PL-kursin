baa9c097633e62a320b4fe491fd9e1542c93339e
dron727 <andreykursin409@gmail.com>
2024-10-15 21:05:27 +0300

    Update практика1.py

--- практика1.py
+++ практика1.py
@@ -1,13 +1,7 @@
-import math
+name = input("Введите фамилию и имя: ")
+years = input("Сколько вам лет? ")
+locations = input("Где вы житвёте? ")
 
-x = (16.55 * 10 ** (-3))
-y = (-2.75)
-z = (0.15)
-
-s1 = math.sqrt(10*((x**(1/3)+x**(y+2))))
-
-s2 = (math.asin(z) ** 2) - (abs(x-y))
-
-s = s1 * s2
-
-print(s)
+print(name)
+print(years)
+print(locations)
